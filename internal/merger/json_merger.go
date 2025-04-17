9a5126f27d52f93974d56d4cc8f5492a521e8f86
Excel619 <excel8392@gmail.com>
2025-04-16 18:16:55 -0600

    Fixed bug with json not supporting top-level lists

--- internal/merger/json_merger.go
+++ internal/merger/json_merger.go
@@ -11,18 +11,33 @@ func NewJSONLoader() ConfigLoader {
 	return &JSONLoader{}
 }
 
-func (m *JSONLoader) Load(path string) (map[string]interface{}, error) {
-	var data map[string]interface{}
+func (m *JSONLoader) Load(path string) (Config, error) {
 	content, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
-	err = json.Unmarshal(content, &data)
-	return data, err
+
+	var raw interface{}
+	if err := json.Unmarshal(content, &raw); err != nil {
+		return nil, err
+	}
+
+	if data, ok := raw.(map[string]interface{}); ok {
+		return data, nil
+	}
+
+	return map[string]interface{}{"$root": raw}, nil
 }
 
-func (m *JSONLoader) Write(path string, data map[string]interface{}) error {
-	out, err := json.MarshalIndent(data, "", "  ")
+func (m *JSONLoader) Write(path string, data Config) error {
+	var toWrite interface{} = data
+	if len(data) == 1 {
+		if val, ok := data["$root"]; ok {
+			toWrite = val
+		}
+	}
+
+	out, err := json.MarshalIndent(toWrite, "", "  ")
 	if err != nil {
 		return err
 	}
