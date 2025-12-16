94fe08aa381f557555e6c7f053a6ff6a92872a59
Excel619 <excel8392@gmail.com>
2025-12-16 16:45:37 -0700

    Added -verbose flag for more logs to debug

--- internal/walker/walker.go
+++ internal/walker/walker.go
@@ -13,13 +13,13 @@ import (
 // The overlayDirs are processed in order: the earliest overlay that has a file
 // is used as that file's "base," and any later overlays with the same file
 // override/merge accordingly. A file present only in later overlays still gets written.
-func Walk(overlayDirs []string, outputDir string) error {
+func Walk(overlayDirs []string, outputDir string, verbose bool) error {
 	if len(overlayDirs) == 0 {
 		return fmt.Errorf("at least one overlay dir is required")
 	}
 
 	// Gather *all* files from *all* overlays into a map:
-	pathMap, err := gatherAllPaths(overlayDirs)
+	pathMap, err := gatherAllPaths(overlayDirs, verbose)
 	if err != nil {
 		return err
 	}
@@ -31,6 +31,9 @@ func Walk(overlayDirs []string, outputDir string) error {
 		var config []merger.Config
 		for _, idx := range indices {
 			overlayPath := filepath.Join(overlayDirs[idx], relPath)
+			if verbose {
+				fmt.Printf("Loading %s from overlay %d: %s\n", relPath, idx, overlayPath)
+			}
 			overlayData, err := loader.Load(overlayPath)
 			if err != nil {
 				return err
@@ -43,10 +46,16 @@ func Walk(overlayDirs []string, outputDir string) error {
 				absOverlay, err1 := filepath.Abs(overlayDirs[idx])
 				absOutput, err2 := filepath.Abs(outputDir)
 				if err1 == nil && err2 == nil && absOverlay == absOutput {
+					if verbose {
+						fmt.Printf("Skipping %s (only present in output dir)\n", relPath)
+					}
 					continue
 				}
 			}
 
+			if verbose {
+				fmt.Printf("Merging %d configs for %s\n", len(config), relPath)
+			}
 			data := merger.Merge(config)
 
 			outPath := filepath.Join(outputDir, relPath)
@@ -56,6 +65,9 @@ func Walk(overlayDirs []string, outputDir string) error {
 
 			if err := loader.Write(outPath, data); err != nil {
 				return err
+			}
+			if verbose {
+				fmt.Printf("Written %s to %s\n", relPath, outPath)
 			}
 		}
 	}
@@ -71,9 +83,12 @@ type ConfigFile struct {
 // gatherAllPaths walks all overlays in order and accumulates a map:
 //
 //	relativePath -> list of overlay indices that contain that file.
-func gatherAllPaths(overlayDirs []string) (map[string]*ConfigFile, error) {
+func gatherAllPaths(overlayDirs []string, verbose bool) (map[string]*ConfigFile, error) {
 	pathMap := make(map[string]*ConfigFile)
 	for i, dir := range overlayDirs {
+		if verbose {
+			fmt.Printf("Scanning overlay %d: %s\n", i, dir)
+		}
 		err := filepath.Walk(dir, func(fullPath string, info fs.FileInfo, err error) error {
 			if err != nil || info.IsDir() {
 				return err // skip dirs or errors
@@ -82,6 +97,10 @@ func gatherAllPaths(overlayDirs []string) (map[string]*ConfigFile, error) {
 			rel, err := filepath.Rel(dir, fullPath)
 			if err != nil {
 				return err
+			}
+
+			if verbose {
+				fmt.Printf("Found file %s in overlay %d\n", rel, i)
 			}
 
 			configFile, ok := pathMap[rel]
@@ -94,6 +113,9 @@ func gatherAllPaths(overlayDirs []string) (map[string]*ConfigFile, error) {
 				ext := filepath.Ext(rel)
 				loader, err := merger.ForExtension(ext)
 				if err != nil {
+					if verbose {
+						fmt.Printf("Skipping %s (unsupported extension: %s)\n", rel, ext)
+					}
 					delete(pathMap, rel)
 					return nil
 				}
